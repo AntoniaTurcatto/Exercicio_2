3205b16909a9f76aa72b47b766a333e9c4ffd154
Antônia Turcatto <lucasturcatto.va048@academico.ifsul.edu.br>
2023-03-28 20:33:22 -0300

    aaa

---
+++ src/main/kotlin/Exercicio2.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>){
+    var inteiro :Int = 10
+    val constante : Int =5
+    // variáveis podem ser alteradas quantas vezes quiser
+    //constantes não podem ser alteradas
+    println("inteiro: $inteiro\nconstante: $constante")
+}
